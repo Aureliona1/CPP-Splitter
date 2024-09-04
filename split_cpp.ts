cbfb7aac96bd1f22ae2426a3acfb5dd3cdd7c000
Aurellis <62498557+Aureliona1@users.noreply.github.com>
2024-09-04 17:51:42 +0930

    better format and error checking

--- split_cpp.ts
+++ split_cpp.ts
@@ -4,31 +4,39 @@ export function splitCPP(file: string, outputFolder?: string) {
 	if (outputFolder) {
 		ensureDirSync(outputFolder);
 	}
-
 	// Get the sections
 	const sections: string[][] = [];
 	while (src.length > 1) {
-		const start = new RegExp(/\/\/ \w*\.\w+/).exec(src);
-		const end = new RegExp(/\/\/ end/).exec(src);
-		const content = src
-			.slice(start?.index ?? 0, end?.index ? end.index + 6 : 1)
-			.replaceAll(new RegExp(/\/\/ \w*\.\w+\s{2}/, "g"), "")
-			.replaceAll(new RegExp(/\s{2}\/\/ end/, "g"), "");
-		if (content.length > 1) {
-			sections.push([(start?.[0] ?? "").replace("// ", ""), content]);
+		const start = /\/\/ \w*\.(?:cpp|h)\s+/.exec(src);
+		const end = /\/\/ end/.exec(src);
+		// If there are no start points in the script then we can just stop the function.
+		if (!start) break;
+		// If the endpoint is somehow before a valid startpoint (i.e., startpoint formatted wrong), remove the endpoint and move on
+		if ((end?.index ?? 0) + 6 < start.index) {
+			const arr = Array.from(src);
+			arr.splice(end?.index ?? 0, (end?.index ?? 0) + 6);
+			src = arr.join("");
+		} else {
+			// In the case that we have a valid start and end
+			const content = src
+				.slice(start?.index ?? 0, end?.index ? end.index + 6 : 1)
+				.replaceAll(new RegExp(/\/\/ \w*\.(?:cpp|h)\s*/, "g"), "")
+				.replaceAll(new RegExp(/\s*\/\/ end/, "g"), "");
+			if (content.length > 1) {
+				sections.push([(start?.[0] ?? "").replace("// ", "").trim(), content]); // For a while there wasn't a .trim() here which kept making files with a space at the end of the name :(
+			}
+			const arr = Array.from(src);
+			arr.splice(start?.index ?? 0, end?.index ? end.index + 6 - (start?.index ?? 0) : 1);
+			src = arr.join("");
 		}
-		const arr = Array.from(src);
-		arr.splice(start?.index ?? 0, end?.index ? end.index + 6 - (start?.index ?? 0) : 1);
-		src = arr.join("");
 	}
-
 	// Get header files and add imports
 	const hfiles: string[] = [];
 	for (let i = 0; i < sections.length; i++) {
 		// header guards
 		if (/.*\.h/.test(sections[i][0])) {
 			const defName = sections[i][0].toLocaleUpperCase().replace(".", "_");
-			sections[i][1] = "#ifndef " + defName + "\n" + "#define " + defName + "\n" + sections[i][1] + "\n" + "#endif";
+			sections[i][1] = "#ifndef " + defName + "\n#define " + defName + "\n" + sections[i][1] + "\n#endif";
 			hfiles.push(sections[i][0]);
 		}
 	}
@@ -36,7 +44,7 @@ export function splitCPP(file: string, outputFolder?: string) {
 	for (let i = 0; i < sections.length; i++) {
 		if (!/.*\.h/.test(sections[i][0])) {
 			for (let j = 0; j < hfiles.length; j++) {
-				sections[i][1] = "#include " + `\"${hfiles[j]}\"` + "\n" + sections[i][1];
+				sections[i][1] = `#include \"${hfiles[j]}\"\n` + sections[i][1];
 			}
 		}
 		Deno.writeTextFileSync(outputFolder ? outputFolder + "/" + sections[i][0] : sections[i][0], sections[i][1]);
