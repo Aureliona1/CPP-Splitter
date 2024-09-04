6b8f82e9abf3508b8fb8145344cfb0b41924da64
Aurellis <62498557+Aureliona1@users.noreply.github.com>
2024-09-04 13:36:47 +0930

    readme

--- split_cpp.ts
+++ split_cpp.ts
@@ -1,34 +1,4 @@
-/**
-My uni uses a stupid structure for cpp where classes have to be defined in individual files and declared in another set of files.
-
-But I want to do it all in one file to keep track of everything.
-
-This function should split it up into separate files.
-
-
-cpp files must be formatted as 
-
-```cpp
-
-// filename.extension
-content;
-// end
-// filename.extension
-content;
-// end
-
-```
-etc...
-
-It doesn't account for includes or imports unless you put them in one of the sections.
-Also, if a header file requires another header file, the import will not be added.
-
-It is recommmended to go thorugh all your generated files and fix them after running.
-
-*/
-
 import { ensureDirSync } from "https://deno.land/std@0.110.0/fs/ensure_dir.ts";
-
 export function splitCPP(file: string, outputFolder?: string) {
 	let src = Deno.readTextFileSync(file).replaceAll("\r\n", " ");
 	if (outputFolder) {
